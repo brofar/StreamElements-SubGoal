2d2e400f81df8cbd8531c62fe238fcb18e47adc5
Behfar Lotfizadeh <behfar@otafest.com>
2021-12-24 13:47:43 -0700

    Fix count on gifted subs

--- js.js
+++ js.js
@@ -1,30 +1,36 @@
-function calculateGoal (obj) {
-    let fieldData = obj.detail.fieldData;
-  let goal = fieldData.subGoal;
-  let data = obj["detail"]["session"]["data"];
-  let subs = data["subscriber-total"]["count"];
+var goal = 1;
+var subs = 0;
 
+function calculateGoal () {
   let progress = Math.floor((subs / goal) * 100);
-  
   let progressBar = document.getElementById('subProgress');
   progressBar.style.width = progress + "%";
-}
-
-window.addEventListener('onEventReceived', function (obj) {
-    if (!obj.detail.event) {
-      return;
-    }
-    const listener = obj.detail.listener.split("-")[0];
-
-    if (listener === 'subscriber') {
-      calculateGoal(obj);
-    }
-});
-
-window.addEventListener('onWidgetLoad', function (obj) {
-    let fieldData = obj.detail.fieldData;
-  let message = document.getElementById('message');
-  message.innerText = fieldData.goalText;
+  }
   
-  calculateGoal(obj);
-});
+  window.addEventListener('onEventReceived', function (obj) {
+      if (!obj.detail.event) return;
+      const listener = obj.detail.listener.split("-")[0];
+  
+      if (listener === 'subscriber') {
+        //subs += obj.detail.event.amount;
+        // Only add one. Multiple gift subs trigger the regular sub event x times.
+        ++subs;
+        calculateGoal();
+      }
+  });
+  
+  window.addEventListener('onWidgetLoad', function (obj) {
+  	let fieldData = obj.detail.fieldData;
+    let message = document.getElementById('message');
+    
+    // Init goal
+    goal = fieldData.subGoal;
+    
+    // Init goal message
+    message.innerText = fieldData.goalText;
+    
+    // Get the initial number of subscribers
+    subs = obj["detail"]["session"]["data"]["subscriber-total"]["count"];
+    
+    calculateGoal();
+  });
